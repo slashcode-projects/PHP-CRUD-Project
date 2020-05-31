4f5c5ad42a474cde532412b845341af051ea244c
slashcode-projects <51401181+slashcode-projects@users.noreply.github.com>
2020-05-31 14:06:39 +0530

    register code

--- 4th Video CRUD Project/models/User.php
+++ 4th Video CRUD Project/models/User.php
@@ -15,5 +15,22 @@ class User extends CI_Model{
         $this->db->where('id',$userId);
         return $user = $this->db->get('users')->row_array();
     }
+    //insert user details
+    public function insert($data = array()) { 
+        if(!empty($data)){ 
+            if(!array_key_exists("created", $data)){ 
+                $data['created'] = date("Y-m-d H:i:s"); 
+            } 
+             
+            $insert = $this->db->insert($this->table, $data); 
+             
+            return $insert?$this->db->insert_id():false; 
+        } 
+        return false; 
+    }
 
+    public function getRows($email){
+     $this->db->where('email',$email);
+        return $user = $this->db->get('users')->row_array();   
+    }
 }
